a735dbdd335ca2acfd09b89ebc8fb20423047297
moaaz97 <moaaz.briek97@gmail.com>
2023-03-19 22:18:26 +0200

    Editted the database name

--- Includes/config.php
+++ Includes/config.php
@@ -1,11 +1,9 @@
 <?php
-    $connection = new PDO('mysql:host=localhost;dbname=php_rest_api', 'root', '');
+    $connection = new PDO('mysql:host=localhost;dbname=phprest', 'root', '');
 
     //set PDO Attributes
     $connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $connection->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    const APP_NAME = 'PHP REST API';
 ?>
 
